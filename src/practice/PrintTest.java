b4d0aa02534a47d236957edc2bda98c1c4237b46
mdalihayder <ali_usa@outlook.com>
2018-10-24 20:25:14 -0400

    print

--- src/practice/PrintTest.java
+++ src/practice/PrintTest.java
@@ -3,7 +3,7 @@ package practice;
 public class PrintTest {
 
 	public static void main(String[] args) {
-		 System.out.print("Counting down");
+		 System.out.print("Counting down start");
          
          for (int i = 0; i <10; i++) {
         	 
